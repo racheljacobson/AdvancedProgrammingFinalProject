0d89a82ca82b0b8c44dcc585431316fbce1da094
racheljacobson <55513903+racheljacobson@users.noreply.github.com>
2021-12-09 00:28:18 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,54 @@
+package com.sw409.studentlibrarydemo.models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity(name = "studentTable")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Integer stuid;
+	String name; //do we want first and last names?
+	
+	@OneToMany(mappedBy = "student")
+	private List<Library> books;
+
+	public Integer getStuid() {
+		return stuid;
+	}
+
+	public void setStuid(Integer stuid) {
+		this.stuid = stuid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Library> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Library> books) {
+		this.books = books;
+	}
+	
+	public Student() {
+		
+	}
+	
+	public Student(Integer stuid, String name) {
+		this.stuid = stuid;
+		this.name = name;
+	}
+
+}
---
+++ StudentLibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sw409.studentlibrarydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLibraryDemoApplication.class, args);
+	}
+
+}
