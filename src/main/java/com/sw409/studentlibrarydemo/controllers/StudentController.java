e6af2b2098973844aa69719dd59faa3bcf0b4d5e
racheljacobson <55513903+racheljacobson@users.noreply.github.com>
2021-12-15 18:08:04 -0500

    Additions to CRUD operations

--- src/main/java/com/sw409/studentlibrarydemo/controllers/StudentController.java
+++ src/main/java/com/sw409/studentlibrarydemo/controllers/StudentController.java
@@ -1,9 +1,15 @@
 package com.sw409.studentlibrarydemo.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sw409.studentlibrarydemo.models.Student;
@@ -14,10 +20,29 @@ public class StudentController {
 	@Autowired
 	StudentService stuService;
 
-	
+	//CREATE object--> POST mapping
+	@PostMapping("api/student") 
+	public Student addStudent(@RequestBody Student stu) {
+		return stuService.addStudent(stu);
+	}
+
+	//READ --> GET mapping
 	@GetMapping("api/student")
 	public ArrayList<Student> getAllStudents() {
 		return stuService.getAllStudents();
 	}
+
+	//UPDATE --> PATCH mapping
+	@PatchMapping("api/student/{stuid}")
+	public void updateStudent(@PathVariable("stuid") Integer id, @RequestBody Student stu){
+		stuService.updateStudent(id, stu);
+	}
+
+	//DELETE --> DELETE mapping
+	@DeleteMapping("api/student/{stuid}")
+	public void deleteStudent(@PathVariable("stuid") Integer id) {
+		stuService.deleteStudent(id);
+	}
+
 
 }
--- src/main/java/com/sw409/studentlibrarydemo/services/StudentService.java
+++ src/main/java/com/sw409/studentlibrarydemo/services/StudentService.java
@@ -1,6 +1,7 @@
 package com.sw409.studentlibrarydemo.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,34 @@ import com.sw409.studentlibrarydemo.repositories.StudentRepository;
 public class StudentService {
 	@Autowired
 	StudentRepository stuRepo;
+
+	List<Student> stuList = new ArrayList<>();
 	
-	
-	
+	//CREATE --> POST
+	public Student addStudent(Student stu) {
+        stuList.add(stu);
+		return stuRepo.save(stu);
+    }
+
+	//READ --> GET
 	public ArrayList<Student> getAllStudents() {
 		return (ArrayList<Student>) stuRepo.findAll();
 	}
+
+	//UPDATE --> PATCH 
+	public void updateStudent(Integer id, Student stu) {
+		Student oldInfoStudent = stuRepo.findById(id).get(); //get student by id
+		oldInfoStudent.setBooks(stu.getBooks()); //updates book list for that student
+		stuRepo.save(stu);
+	}
+
+	//DELETE
+	public void deleteStudent(Integer id) {
+		stuRepo.deleteById(id); //deletes a student from the list 
+	}
+
+
+
+    
 
 }
