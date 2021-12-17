1b9052c498f61efe2f459baf12296f482a1790ae
racheljacobson <55513903+racheljacobson@users.noreply.github.com>
2021-12-16 20:50:49 -0500

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sw409.studentlibrarydemo.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sw409.studentlibrarydemo.models.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Integer>{
-
-}
